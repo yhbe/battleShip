3f47df30d669b44cd4ae26e0334056130d58d43d
SHEF <Shefqetmush@gmail.com>
2022-12-17 14:45:15 -0500

    create Ship factor and tests

--- src/createShip.js
+++ src/createShip.js
@@ -1,3 +1,13 @@
-export function sum(a, b) {
-  return a + b;
+export function createShip(length) {
+  return { length: length, hits: 0, sunk: false, hit, sunked };
+  function hit() {
+    if (this.hits === this.length - 1) {
+      this.hits++;
+      return this.sunked();
+    }
+    return (this.hits += 1);
+  }
+  function sunked() {
+    return (this.sunk = true);
+  }
 }
--- src/createShip.test.js
+++ src/createShip.test.js
@@ -1,5 +1,20 @@
-import { sum } from "./createShip";
+import { createShip } from "./createShip";
 
-test("Adds two numbers", () => {
-  expect(sum(1, 2)).toBe(3);
+test("Return of new createShip", () => {
+  expect(createShip(4)).toMatchObject({ length: 4, hits: 0, sunk: false });
+});
+//
+
+test("Hits on aircraft carrier work", () => {
+  let aircraftCarrier = createShip(4);
+  expect(aircraftCarrier.hit()).toBe(1);
+});
+
+test("Sink aircraft carrier", () => {
+  let aircraftCarrier = createShip(4);
+  aircraftCarrier.hit();
+  aircraftCarrier.hit();
+  aircraftCarrier.hit();
+  aircraftCarrier.hit();
+  expect(aircraftCarrier.sunk).toBe(true);
 });
