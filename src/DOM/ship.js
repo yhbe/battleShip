62fa30e60da950e037ad84b2160fd4191b66ed18
SHEF <Shefqetmush@gmail.com>
2022-12-18 16:05:17 -0500

    ship test and function created

---
+++ src/DOM/ship.js
@@ -0,0 +1,10 @@
+export function Ship(length) {
+  return { length: length, hits: 0, sunk: false, hit };
+
+  function hit() {
+    if (this.hits === length - 1) {
+      return (this.sunk = true);
+    }
+    return this.hits++;
+  }
+}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,31 @@
+import { Ship } from "../DOM/ship";
+
+describe("ship", () => {
+  let destroyer;
+
+  beforeEach(() => {
+    destroyer = new Ship(5);
+  });
+
+  test("new Ship returns a object", () => {
+    expect(destroyer).toMatchObject({});
+  });
+
+  test("Has a length property", () => {
+    expect(destroyer.length).toBe(5);
+  });
+
+  test("number of times its been hit property", () => {
+    destroyer.hit();
+    expect(destroyer.hits).toBe(1);
+  });
+
+  test("Whether or not its been sunk", () => {
+    destroyer.hit();
+    destroyer.hit();
+    destroyer.hit();
+    destroyer.hit();
+    destroyer.hit();
+    expect(destroyer.sunk).toBe(true);
+  });
+});
