9f75449619ec447cd866f7f6888a04856885896d
SHEF <Shefqetmush@gmail.com>
2022-12-18 16:09:46 -0500

    gameboard test

---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,13 @@
+import { Gameboard } from "../DOM/gameboard";
+
+describe("testing gameboard", () => {
+  let board;
+
+  beforeEach(() => {
+    board = new Gameboard();
+  });
+
+  test("Gameboard can place a ship at coordinates", () => {
+    expect();
+  });
+});
