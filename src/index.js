c425c2b380b62da0579ac5c714b12c43a53ea01a
SHEF <Shefqetmush@gmail.com>
2022-12-20 20:51:15 -0500

    working on rendering gameboard

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import { Gameboard } from "./Components/gameboard";
 import "./style.css";
 
-const div = document.querySelector("div");
+function renderBoards() {
+  const div = document.querySelector("div");
+  let playerBoard = new Gameboard();
+  for (let elem of playerBoard.board) {
+    let squareDiv = document.createElement("div");
+    squareDiv.className = `${elem.slice(0, -1)} squareDiv`;
+    div.append(squareDiv);
+  }
+}
+renderBoards();
